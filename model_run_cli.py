157fa080af6bbbe5f430fcb76a752e1e4df0cf59
Chad Rosenberg <abditus@gmail.com>
2023-10-04 09:30:18 -0700

    model_run_cli.py fix

--- model_run_cli.py
+++ model_run_cli.py
@@ -12,18 +12,15 @@ CONFIG_FOLDER = sys.argv[1]
 MULTI_PROCESS = True
 MULTI_PROCESS_CONCURRENT = 2
 
-# Get the list of config files from the folder specified on the command line
-#first remove __pycache__
-all_files = os.listdir(CONFIG_FOLDER)
-if '__pycache__' in all_files:
-    all_files.remove('__pycache__')
-config_list = [ os.path.join(CONFIG_FOLDER, file) for file in all_files]
+if __name__ == '__main__':
+    # Get the list of config files from the folder specified on the command line
+    config_list = [ os.path.join(CONFIG_FOLDER, file) for file in os.listdir(CONFIG_FOLDER) ]
 
-if MULTI_PROCESS:
-    with Pool(MULTI_PROCESS_CONCURRENT) as pool:
-        run_configs = [ PollingModelConfig.load_config(run_config) for run_config in config_list ]
-        processed = pool.map(model_run.run_on_config, run_configs)
-else:
-    for config_file in config_list:
-        run_config = PollingModelConfig.load_config(config_file)
-        model_run.run_on_config(run_config)
+    if MULTI_PROCESS:
+        with Pool(MULTI_PROCESS_CONCURRENT) as pool:
+            run_configs = [ PollingModelConfig.load_config(run_config) for run_config in config_list ]
+            processed = pool.map(model_run.run_on_config, run_configs)
+    else:
+        for config_file in config_list:
+            run_config = PollingModelConfig.load_config(config_file)
+            model_run.run_on_config(run_config)
