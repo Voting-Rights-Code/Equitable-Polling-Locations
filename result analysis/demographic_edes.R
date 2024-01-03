f0301fcac2ac60eac87b962c888a104bfe7fa31d
Susama Agarwala <susama.agarwala@covidalliance.org>
2024-01-03 11:11:55 -0500

    ede polots not refactored and working

--- result analysis/demographic_edes.R
+++ result analysis/demographic_edes.R
@@ -1,34 +1,53 @@
 library(data.table)
 library(ggplot2)
 library(stringr)
+library(here)
 
 #######
 #Change directory
 #######
-setwd('~')
-setwd('../../Voting Rights Code/Equitable-Polling-Locations')
+setwd(here())
+#setwd('../')
 
 #######
-#Set Constants
+#Check that location and folders valid
 #######
+check_location_valid <- function(location, config_folder){
+	#raise error if config folder does not start with location
+	if (!grepl(paste0('^', location), config_folder)){
+    stop('Given config folder does not start with the given location')
+}
+}
 
-config_folder = 'Gwinnett_GA_no_school_no_fire_configs'
+check_config_folder_valid <- function(config_folder){
+	#raise error if config folder not a directory
+	if (!(config_folder %in% list.files())){
+    	stop('Config folder does not exist')
+}
+}
 
 ######
-#Read in results
+#Functions to read in results
 ######
-#local run
-process_results <-function(config_folder, result_type){
+
+combine_results <-function(config_folder, result_type){
+	#combine all the data of a certain type 
+	#(ede, precinct, residence, result)
+	#from indicated config_folder and output a df
+	#config_folder, result_type: string
+	#returns: data frame
+
 	#select which results we want
-	files <- list.files('Gwinnett_GA_results/')
+	result_folder <-paste(location, 'results/', sep = '_')
+	files <- list.files(result_folder)
 	files <- files[grepl(config_folder, files) &grepl(result_type, files)]
-	file_path <- paste0('Gwinnett_GA_results/', files)
+	file_path <- paste0(result_folder, files)
 	df_list <- lapply(file_path, fread)
 
-	#name the data
-	config_names_long <- lapply(files, function(x){gsub(".*Gwinnett_config_\\s*|.csv*", "", x)})	
+	#name the data from the file names
+	config_names_long <- lapply(files, function(x){gsub(paste0('.*', county_config_,"\\s*|.csv*"), "", x)})	
 	config_names <- lapply(config_names_long, function(x){gsub(paste0('_',result_type), "", x)})
-	file_path <- paste0('Gwinnett_GA_results/', files)
+	file_path <- paste0(result_folder, files)
 	df_list <- lapply(file_path, fread)
 	names(df_list) <- config_names
 
@@ -47,168 +66,170 @@ process_results <-function(config_folder, result_type){
 	return(big_df)
 }
 
+read_result_data<- function(config_folder){
+	#read in and format all the results data assocaited to a 
+	#given config folder.
+	#config_folder: string
+	#returns: list(ede_df, precinct_df, residence_df, result_df)
+	
+	#combine all files with a descriptor column attached
+	ede_df<- combine_results(config_folder, 'edes')
+	precinct_df<- combine_results(config_folder, 'precinct_distances')
+	residence_df<- combine_results(config_folder, 'residence_distances')
+	result_df<- combine_results(config_folder, 'result')
 
-#combine all files with a descriptor column attached
-ede_df<- process_results(config_folder, 'edes')
-precinct_df<- process_results(config_folder, 'precinct_distances')
-residence_df<- process_results(config_folder, 'residence_distances')
-result_df<- process_results(config_folder, 'result')
+	#label descriptors with polls and residences
+	num_polls <- precinct_df[ , .(num_polls = .N/6), by = descriptor]
+	num_residences <- residence_df[ , .(num_residences = .N/6), by = descriptor]
+	nums_to_join <- merge(num_polls, num_residences, all = T)
 
-#label descriptors with polls and residences
-num_polls <- precinct_df[ , .(num_polls = .N/6), by = descriptor]
-num_residences <- residence_df[ , .(num_residences = .N/6), by = descriptor]
-nums_to_join <- merge(num_polls, num_residences, all = T)
+	ede_df <- merge(ede_df,nums_to_join, all.x = T)
+	precinct_df <- merge(precinct_df,nums_to_join, all.x = T)
+	residence_df <- merge(residence_df,nums_to_join, all.x = T)
+	result_df <- merge(result_df,nums_to_join, all.x = T)
 
-ede_df <- merge(ede_df,nums_to_join, all.x = T)
-precinct_df <- merge(precinct_df,nums_to_join, all.x = T)
-residence_df <- merge(residence_df,nums_to_join, all.x = T)
-result_df <- merge(result_df,nums_to_join, all.x = T)
+	return(list(ede_df, precinct_df, residence_df, result_df))
+}
 
-#create the datatables for the originals for later
-ede_orig_df<- process_results('Gwinnett_GA_configs', 'edes')
-precinct_orig_df<- process_results('Gwinnett_GA_configs', 'precinct_distances')
-residence_orig_df<- process_results('Gwinnett_GA_configs', 'residence_distances')
-result_orig_df<- process_results('Gwinnett_GA_configs', 'result')
-
-ede_orig_df<-ede_orig_df[grepl('original', descriptor),]
-precinct_orig_df<-precinct_orig_df[grepl('original', descriptor),]
-residence_orig_df<-residence_orig_df[grepl('original', descriptor),]
-result_orig_df<-result_orig_df[grepl('original', descriptor),]
-
-num_polls <- precinct_orig_df[ , .(num_polls = .N/6), by = descriptor]
-num_residences <- residence_orig_df[ , .(num_residences = .N/6), by = descriptor]
-nums_to_join <- merge(num_polls, num_residences, all = T)
-
-ede_orig_df <- merge(ede_orig_df,nums_to_join, all.x = T)
-precinct_orig_df <- merge(precinct_orig_df,nums_to_join, all.x = T)
-residence_orig_df <- merge(residence_orig_df,nums_to_join, all.x = T)
-result_orig_df <- merge(result_orig_df,nums_to_join, all.x = T)
-
+check_run_validity <- function(config, orig){
+	#Input: A pair of result_dfs (each corresponding to a config folder) that should have the same number of matched residences
+	#Check that they do both within and across data frames
+	#Else return an error
+	
+	unique_num_residences <- c(length(unique(config$num_residences)), length(unique(orig$num_residences)))
+	if (any(unique_num_residences) != 1){
+		cat('The following indices corresond to bad runs with different numbers of residences', which(unique_num_residences != 1))
+		stop('Not all runs in the config folder match the same number of residences')
+	} else {#else all(unique_num_residences) == 1
+		num_residences <- c(unique(config$num_residences), unique(orig$num_residences))
+		if (length(unique(num_residences)) != 1) {
+			cat('The number of residences matched in each config is', num_residences)
+			stop('Not all the config folders under consideration match the same number of residences')
+		}
+	}
+}
 
 #######
-#plot edes
+#functions to make plots
 #######
-plot_folder = paste0('result analysis/', config_folder)
-setwd(plot_folder)
 
-ggplot(ede_df[level =='expanded', ]
-	, aes(x = num_polls, y = y_EDE, 
-		group = demographic, color = demographic, shape = demographic)) +
-		geom_line()+ geom_point() +
-	geom_point(data = ede_df[grepl('original', descriptor), ], aes(x = num_polls, y = y_EDE, 
-		group = demographic, color = demographic, shape = demographic)) + 
-	labs(x = 'Number of polls', y = 'Equity weighted distance (m)')
-
-ggsave('demographic_edes_with_originals.png')
-
+plot_demographic_edes<-function(ede_df){
 ggplot(ede_df[grepl('no_bg', descriptor), ], aes(x = num_polls, y = y_EDE, 
 		group = demographic, color = demographic, shape = demographic)) +
 		geom_line()+ geom_point()+ 
 		labs(x = 'Number of polls', y = 'Equity weighted distance (m)')
-
 ggsave('demographic_edes.png')
+}
 
-at_most_11 <- rbind(ede_orig_df, ede_df[descriptor == 'no_bg_11', ])
-descriptor_order <- c('no_bg_11', 'original_2022', 'original_2020')
-ggplot(at_most_11, aes(x = descriptor, y = y_EDE, 
+plot_original_optimized <- function(config_ede, orig_ede){	
+	#makes two plots, one showing the y_ede differences between the actual positioning and an equivalent optimized run; the other doing the same but with average distances
+
+	#select the relevant optimized runs
+	orig_num_polls <- unique(orig_ede$num_polls)
+	optimized_run_dfs <- config_ede[num_polls %in% orig_num_polls]
+	orig_and_optimal <- rbind(optimized_run_dfs, orig_ede)
+	descriptor_order <- unique(orig_and_optimal$descriptor)
+	#plot with y_EDE
+	ggplot(orig_and_optimal, aes(x = descriptor, y = y_EDE, 
 		group = demographic, color = demographic, shape = demographic)) +
 		geom_point(aes(x = factor(descriptor, level = descriptor_order), ), size = 9) +
 		labs(x = 'Optimization run', y = 'Equity weighted distance (m)')
-ggsave('at_most_11.png')
-
-
-ggplot(at_most_11, aes(x = descriptor, y = y_EDE, 
+	ggsave('orig_and_optimal.png')
+	#polot with avg_dist
+	ggplot(orig_and_optimal, aes(x = descriptor, y = avg_dist, 
 		group = demographic, color = demographic, shape = demographic)) +
 		geom_point(aes(x = factor(descriptor, level = descriptor_order), size = 9)) +
-		labs(x = 'Optimization run', y = 'Equity weighted distance (m)')+
-		lims(y = c(3500, 6250))
-ggsave('at_most_11_for_comp_with_avg_dist.png')
+		labs(x = 'Optimization run', y = 'Average distance (m)')
+	ggsave('orig_and_optimal_avg.png')
+}
 
-ggplot(at_most_11, aes(x = descriptor, y = avg_dist, 
-		group = demographic, color = demographic, shape = demographic)) +
-		geom_point(aes(x = factor(descriptor, level = descriptor_order), size = 9)) +
-		labs(x = 'Optimization run', y = 'Average distance (m)')+
-		lims(y = c(3500, 6250))
-ggsave('at_most_11_avg.png')
-
-
-good_runs_pops <- ede_df[num_polls != 50, 
-			][!(descriptor %in% c('full_27', 'full_30')),
-			][demographic == 'population', ] 
-
-ggplot(good_runs_pops[level %in% c('expanded', 'full'), ], aes(x =  num_polls, y = y_EDE, group = level, 
-			color = level))+
+plot_population_edes <- function(ede_df){	
+	#plots just the y_edes for the population as a whole
+	ggplot(ede_df[demographic == 'population', ], aes(x =  num_polls, y = y_EDE))+
 		geom_line()+ geom_point()+
 		labs(x = 'Number of polls', y = 'Equity weighted distance (m)')
 
-ggsave('demographic_edes_expanded_full.png')
+	ggsave('population_edes.png')
+}
 
-ggplot(ede_df[demographic == 'population', ], aes(x =  num_polls, y = y_EDE))+
-		geom_line()+ geom_point()+
-		labs(x = 'Number of polls', y = 'Equity weighted distance (m)')
-
-ggsave('population_edes.png')
-
-ggplot(precinct_df[demographic == 'population',
+plot_precinct_persistence <- function(precinct_df){
+	#a plot showing which precincts are used for which number of polls
+	ggplot(precinct_df[demographic == 'population',
 		], aes(x = num_polls, y = id_dest)) +
 		geom_point(aes(size = demo_pop)) + 
 		labs(x = 'Number of polls', y = 'EV location')
 
-ggsave('expanded_precinct_persistence.png')
+	ggsave('expanded_precinct_persistence.png')
 
-ggplot(precinct_df[demographic != 'population',
+	#also makes a panel of graphs showing which demographics are assigned to each poll
+	ggplot(precinct_df[demographic != 'population',
 		], aes(x = num_polls, y = id_dest)) +
 		geom_point(aes(size = demo_pop)) + 
 		labs(x = 'Number of polls', y = 'EV location') + facet_wrap(~ demographic) +
 		theme(legend.position = c(0.9, 0.2))
 
-ggsave('expanded_precinct_persistence_all.png')
+	ggsave('expanded_precinct_persistence_all.png')
+}
 
-res_pop <- residence_df[demographic == 'population',
+plot_boxplots <- function(residence_df){
+	#make boxplots of the average distances traveled and the y_edes at each run 
+	res_pop <- residence_df[demographic == 'population',
 		]
-ggplot(res_pop, aes(x = num_polls, y = avg_dist, group = descriptor)) +
-	stat_boxplot(geom = "errorbar")+
-	geom_boxplot(outlier.shape = NA) + 
-	scale_y_log10(limits = c(500,10500)) +
-	labs(x = 'Number of polls', y = 'Average distance (m)')
-ggsave('avg_dist_distribution_boxplots.png')
+	#avg distance
+	ggplot(res_pop, aes(x = num_polls, y = avg_dist, group = descriptor)) +
+		stat_boxplot(geom = "errorbar")+
+		geom_boxplot(outlier.shape = NA) + 
+		scale_y_log10(limits = c(500,10500)) +
+		labs(x = 'Number of polls', y = 'Average distance (m)')
+	ggsave('avg_dist_distribution_boxplots.png')
+	}
 
-res_pop_orig_and_22 <- rbind(residence_orig_df, residence_df[descriptor == 'no_bg_11', ])
+plot_orig_ideal_hist <- function(orig_residence_df, residence_df, ideal_num){
+	#make histogram of the average distances traveled and the y_edes in the original and ideal situations 
+	orig_residence_df <- orig_residence_df[demographic == 'population', ]
+	ideal_residence_df <- residence_df[demographic == 'population', ][num_polls == ideal_num, ]
+	res_pop_orig_and_ideal <- rbind( ideal_residence_df, orig_residence_df)
+	location_ <- paste0(location, '_')
+	config_label <- sub(location_, '', config_folder)
+	descriptor_order <- unique(res_pop_orig_and_ideal$descriptor)
 
-ggplot(res_pop_orig_and_22, aes(x = avg_dist, fill = descriptor)) + 
-	#geom_density()	
-	geom_histogram(position = "dodge", alpha = 0.8)+
-	labs(x = 'Average distance traveled to poll (m)', y = 'Number of census blocks') +
-	scale_fill_manual(values=c("red", "blue", "green"), name = "Optimization run ", 
-		labels = c("22 Locations", "2020 locations", "2022 locations"))
+	#avg_distance
+	ggplot(res_pop_orig_and_ideal, aes(x = avg_dist, fill = descriptor)) + 
+		geom_histogram(position = "dodge", alpha = 0.8)+
+		labs(x = 'Average distance traveled to poll (m)', y = 'Number of census blocks') +
+		scale_fill_manual(values=c("red", "blue", "green"), name = "Optimization run ", 
+		labels = descriptor_order)
+		ggsave('avg_dist_distribution_hist.png')
+
+}
+
+
+compare_configs<- function(config_ede, config_folder2){
+	#takes a config folder name, processes the files in it, and compares its mean y_ede to the current one under consideration
+
+	#Check that folder is valid
+	check_location_valid(location, config_folder2)
+	check_config_folder_valid(config_folder2)
+
+	#Read in data
+	config2_df_list <- read_result_data(config_folder2)
+	config2_ede <- config2_df_list[1]
+
+	#label configs
+	location_ <- paste0(location, '_')
+	config1_label <- sub(location_, '', config_folder)
+	config2_label <- sub(location_, '', config2_folder) 
+	config_ede <- config_ede[ , run := config1_label]
+	config2_ede <- config2_ede[ , run := config2_label]
 	
-ggsave('avg_dist_distribution_hist.png')
+	#combine and select the population scores only
+	dt <-rbind(config_ede, config2_ede)
+	dt <- dt[demographic == 'population', ]
+	ggplot(dt, aes(x =  num_polls, y = y_EDE, group = run, 
+			color = run))+
+		geom_line()+ geom_point()+
+		labs(x = 'Number of polls', y = 'Equity weighted distance (m)')
 
+	ggsave(paste0(config1_label, '_vs_', config2_label, '.png'))
+}
 
-res_aa <- residence_df[demographic == 'black'
-		][descriptor %in% c('original_2020', 'original_2022', 'expanded_11', 'expanded_22')]
-ggplot(res_aa, aes(x = avg_dist, color = descriptor)) + 
-	#geom_density()	
-	geom_histogram(position = "identity", alpha = 0.4)
-
-#######
-#make datasets for maps
-#######
-
-#dataset of all possible locations (has lat/lon)
-potential_locations <- fread('datasets/polling/Gwinnett_GA/Gwinnett_GA.csv')
-unique_locations <- potential_locations[, .(dest_lat = unique(dest_lat), dest_lon = unique(dest_lon)), by= id_dest, ]
-
-#dataset of matched locations with only one entry for location
-unique_precincts <- lapply(precinct_df_list, 
-	function(x){x[demographic == 'population', ]})
-unique_precincts$full_50 <- unique_precincts$full_50[ , id_dest:= as.character(id_dest)]
-
-#merge
-geotagged_precincts <- lapply(unique_precincts, function(X){unique_locations[X, on = 'id_dest']})
-
-#write
-setwd('datasets/polling/Gwinnett_GA/model_outputs_for_mapping')
-map_file_names <- paste0(names(geotagged_precincts), '.csv')
-mapply(function(x, y) {fwrite(x, y)}, 
-	geotagged_precincts, map_file_names)
---
+++ result analysis/make_ede_plots.r
@@ -0,0 +1,82 @@
+source('../../result analysis/demographic_edes.R')
+
+#######
+#Set Constants
+#######
+#Location must be part of config folder string
+
+location = 'DeKalb_GA'
+config_folder = 'DeKalb_GA_no_bg_school_configs'
+FFA_poll_number  = 20 #the optimal number of polls that FFA is suggesting for this county
+
+#######
+#Location of original location results
+#and other related constants
+#######
+
+original_locations = paste(location, 'original', 'configs', sep = '_')
+#some values for graph labeling
+county = sub('_.*','',location)
+county_config_ = paste0(county, '_', 'config', '_')
+
+#######
+#Check that location and folders valid
+#######
+
+check_location_valid(location, config_folder)
+check_config_folder_valid(config_folder)
+
+#######
+#Read in data
+#Run this for each of the folders under consideration
+#Recall, output of form: list(ede_df, precinct_df, residence_df, result_df)
+#######
+config_df_list <- read_result_data(config_folder)
+#config_ede_df<- config_df_list[[1]]
+#config_precinct_df<- config_df_list[[2]]
+#config_residence_df<- config_df_list[[3]]
+#config_result_df<- config_df_list[[4]]
+
+orig_df_list <- read_result_data(original_locations)
+#defined as above
+
+#######
+#Check result validity
+#######
+
+#This will return an error (and index) in case of inconsistency
+check_run_validity(config_df_list[[4]], orig_df_list[[4]])
+
+
+#######
+#Plot data
+#######
+plot_folder = paste0('result analysis/', config_folder)
+if (file.exists(file.path(here(), plot_folder))){
+    setwd(file.path(here(), plot_folder))    
+} else{
+    dir.create(file.path(here(), plot_folder))
+    setwd(file.path(here(), plot_folder))
+}
+
+#Plot the edes for all runs in config_folder by demographic
+plot_demographic_edes(config_df_list[[1]])
+
+#Plot the edes for all runs in original_location and equivalent optimization runs by demographic
+plot_original_optimized(config_df_list[[1]], orig_df_list[[1]])
+
+#Plot the edes for all runs in config_folder for the population as a whole
+plot_population_edes(config_df_list[[1]])
+
+#Plot which precincts are used for each number of polls
+plot_precinct_persistence(config_df_list[[2]])
+
+#Boxplots of the average distances traveled and the y_edes at each run in folder
+plot_boxplots(config_df_list[[3]])
+
+#Histgram of the original distributions and that for the desired number of polls
+plot_orig_ideal_hist(orig_df_list[[3]], config_df_list[[3]], FFA_poll_number)
+
+#if you want to compare config_folder runs to a different folder, this plots on a population level
+#config_folder2 = 
+#compare_configs(config_df_list[1], config_folder2)
