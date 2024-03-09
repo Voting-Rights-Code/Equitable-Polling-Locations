abf263ca7039faf929cfa339fdf9859a4d3d3dd7
Susama Agarwala <susama.agarwala@covidalliance.org>
2024-03-09 16:27:12 -0500

    initial Engage_VA plot

--- result analysis/Engage_VA_analysis.r
+++ result analysis/Engage_VA_analysis.r
@@ -17,8 +17,10 @@ source('result analysis/graph_functions.R')
 #######
 #Location must be part of config folder string
 
-location_list = c('Fairfax_County_VA', 'Loudon_County_VA', 'Norfolk_City_VA', 'Virginia_Beach_City_VA')
+location = c('Fairfax_County_VA', 'Loudon_County_VA', 'Norfolk_City_VA', 'Virginia_Beach_City_VA')
 config_folder = 'Engage_VA_2024_configs'
+county = gsub('.{3}$','',location)
+county_config_ = paste0(county, '_', 'config', '_')
 
 
 #######
@@ -33,21 +35,19 @@ check_config_folder_valid(config_folder)
 #Run this for each of the folders under consideration
 #Recall, output of form: list(ede_df, precinct_df, residence_df, result_df)
 #######
-config_df_list <- read_result_data(config_folder)
+config_df_list <- read_result_data(config_folder, 'historical')
 #config_ede_df<- config_df_list[[1]]
 #config_precinct_df<- config_df_list[[2]]
 #config_residence_df<- config_df_list[[3]]
 #config_result_df<- config_df_list[[4]]
 
-orig_df_list <- read_result_data(original_locations)
-#defined as above
-
 #######
 #Check result validity
 #######
 
-#This will return and descriptor case of inconsistency
-bad_runs <- check_run_validity(config_df_list[[4]], orig_df_list[[4]])
+#This will return and descriptor case of inconsistency 
+#(assuming that a config file has multiple counties)
+bad_runs <- sapply(county, function(x){check_run_validity(config_df_list[[4]][grepl(x, descriptor), ])})
 
 #remove any bad runs from the data
 config_df_list <- lapply(config_df_list, function(x){x[!(descriptor %in% bad_runs), ]})
@@ -67,7 +67,7 @@ if (file.exists(file.path(here(), plot_folder))){
 plot_demographic_edes(config_df_list[[1]])
 
 #Plot the edes for all runs in original_location and equivalent optimization runs by demographic
-plot_original_optimized(config_df_list[[1]], orig_df_list[[1]])
+plot_original(config_df_list[[1]])
 
 #Plot the edes for all runs in config_folder for the population as a whole
 plot_population_edes(config_df_list[[1]])
