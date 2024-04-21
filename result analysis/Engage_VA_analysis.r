51e8152a057f5bd060fc47f0be15feb28302a4f7
Susama Agarwala <susama.agarwala@covidalliance.org>
2024-04-21 13:13:11 -0400

    Engage_VA driving redone

--- result analysis/Engage_VA_analysis.r
+++ result analysis/Engage_VA_analysis.r
@@ -15,14 +15,12 @@ source('result analysis/map_functions.R')
 #######
 #Set Constants
 #######
-#Location must be part of config folder string
+#Basic constants for analysis
+#LOCATION must be either a string or list of strings
+#CONFIG_FOLDER must be a string
 
 location = c('Fairfax_County_VA', 'Loudon_County_VA', 'Norfolk_City_VA', 'Virginia_Beach_City_VA')
 config_folder = 'Engage_VA_2024_driving_configs'
-#location = 'Greenville_SC'
-#config_folder = 'Greenville_SC_original_configs'
-county = gsub('.{3}$','',location)
-county_config_ = paste0(county, '_', 'config', '_')
 
 
 #######
@@ -30,7 +28,10 @@ county_config_ = paste0(county, '_', 'config', '_')
 #this also ensures that you are in the right folder to read data
 #######
 
-check_config_folder_valid(config_folder)
+#Does the config folder exist?
+check_config_folder_valid(CONFIG_FOLDER)
+#Does the config folder contain files associated to the location
+check_location_valid(LOCATION, CONFIG_FOLDER)
 
 #######
 #Read in data
@@ -43,31 +44,18 @@ config_df_list <- read_result_data(config_folder, 'historical')
 #config_residence_df<- config_df_list[[3]]
 #config_result_df<- config_df_list[[4]]
 
-config_df_list[[2]][ , .(a = unique(num_polls), b = unique(num_residences)), by = descriptor]
-#######
-#Check result validity
-#######
-
-#This will return and descriptor case of inconsistency 
-#(assuming that a config file has multiple counties)
-bad_runs <- sapply(county, function(x){check_run_validity(config_df_list[[4]][grepl(x, descriptor), ])})
-
-#remove any bad runs from the data
-config_df_list <- lapply(config_df_list, function(x){x[!(descriptor %in% bad_runs), ]})
 
 #######
 #Constants for mapping
 #######
 #set result folder
-result_folder = paste(location, 'results', sep = '_')
+result_folder = paste(LOCATION, 'results', sep = '_')
 
 #get all file names the result_folder with the strings config_folder and 'residence_distances'
 res_dist_list = list.files(result_folder)[grepl('residence_distances', list.files(result_folder))]
-res_dist_list = res_dist_list[grepl(config_folder, res_dist_list)]
+res_dist_list = res_dist_list[grepl(CONFIG_FOLDER, res_dist_list)]
 
 #get avg distance bounds for maps
-if (length(county_config_) >1){
-county_config_ <- county_config_[1]} #cludge. Fix later
 color_bounds <- distance_bounds(config_folder)
 
 
@@ -84,9 +72,8 @@ if (file.exists(file.path(here(), plot_folder))){
 
 
 #Plot the edes for all runs in original_location and equivalent optimization runs by demographic
-#TODO: Give Jenn config_df_list[[1]] for tableau work  
-#plot_original(config_df_list[[1]], scale_bool = F)
-#plot_original_pop_sized(config_df_list[[1]], conf_df_list[[2]])
+
+#join ede data with population data for scaling
 pop_scaled_edes <- ede_with_pop(config_df_list)
 #population scaled graph
 plot_election_edes(pop_scaled_edes, suffix = 'pop_scaled')
@@ -97,49 +84,16 @@ plot_election_edes(config_df_list[[1]], suffix ='')
 #Make maps and cartograms
 #########
 
-mapply(function(x,y, z){make_bg_maps(x, 'map', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
+#Choosing not to do cartograms because of convergence difficulties
 
-mapply(function(x,y, z){make_bg_maps(x, 'cartogram', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
+mapply(function(x,y, z){make_bg_maps(CONFIG_FOLDER, x, 'map', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
 
-mapply(function(x,y, z){make_demo_dist_map(x, 'white', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
+#mapply(function(x,y, z){make_bg_maps(x, 'cartogram', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
 
-mapply(function(x,y, z){make_demo_dist_map(x, 'black', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
+mapply(function(x,y, z){make_demo_dist_map(CONFIG_FOLDER, x, 'white', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
 
-mapply(function(x,y, z){make_demo_dist_map(x, 'population', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
+mapply(function(x,y, z){make_demo_dist_map(CONFIG_FOLDER, x, 'black', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
 
-make_demo_dist_map(res_dist_list[[1]], 'population', result_folder_name = result_folder[1], this_location = location[1])
-
-#######
-#Regression work
-#######
-
-map_folders <- paste0('../../datasets/census/tiger/', location, '/')
-map_files <- paste0(map_folders, list.files(map_folders)[endsWith(list.files(map_folders), 'tabblock20.shp')])
-
-map_data<- lapply(map_files, st_read)
-block_areas <- lapply(map_data, function(x){x[, c('GEOID20', 'ALAND20', 'AWATER20')]})
-if (length(block_areas)> 1){
-    block_areas <- do.call(rbind, block_areas)
-}
-
-regression_data <- merge(config_df_list[[4]], block_areas, by.x = 'id_orig', by.y = 'GEOID20', all.x = T)
-regression_data[ , `:=`(pop_density_m = population/(ALAND20 + AWATER20), pop_density_km = 1e6 *population/(ALAND20 + AWATER20),dist_m = Weighted_dist/ population, pct_white= 100 * white/population, pct_black = 100 *black/population)]
-
-model1 <- lm(dist_m ~ pop_density_km + white + black, data = regression_data, weights = population )
-model2 <- lm(Weighted_dist ~ pop_density + pct_white + pct_black, data = regression_data)
-
-dt1 <- regression_data[ , as.list(coef(lm(dist_m ~ pop_density_km + white + black,  weights = population ))), by = descriptor]
-dt1.1 <- regression_data[ , as.list(coef(lm(dist_m ~ pop_density_km + pct_white + pct_black,  weights = population ))), by = descriptor]
-dt1.2 <- regression_data[ , as.list(coef(lm(dist_m ~ pop_density_km + pct_white,  weights = population ))), by = descriptor]
-dt1.3 <- regression_data[, as.list(coef(lm(dist_m ~ pop_density_km + pct_black,  weights = population ))), by = descriptor]
+mapply(function(x,y, z){make_demo_dist_map(CONFIG_FOLDER, x, 'population', result_folder_name = y, this_location = z)}, res_dist_list, result_folder, location)
 
 
-dt2m <- regression_data[ , as.list(coef(lm(Weighted_dist ~ pop_density_m + pct_white + pct_black ))),  by = descriptor]
-dt2km <- regression_data[ , as.list(coef(lm(Weighted_dist ~ pop_density_km + pct_white + pct_black ))),  by = descriptor]
-
-head(dt1)
-head(dt1.1)
-head(dt1.2)
-head(dt1.3)
-#head(dt2m)
-#head(dt2km)
