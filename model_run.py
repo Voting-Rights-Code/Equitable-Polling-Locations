8e53bd46f83dd43d7c8b26989b75c1b61f0b0197
Daphne Skipper <daphneskipper@daphnes-air.lan>
2024-05-06 06:35:27 -0400

    Gwinnett GA penalty results added

--- model_run.py
+++ model_run.py
@@ -37,7 +37,7 @@ def run_on_config(config: PollingModelConfig, log: bool=False):
     '''
 
     config_file_basename = f'{os.path.basename(config.config_file_path)}'.replace('.yaml','')
-    run_prefix = f'{config.location}_configs.{config_file_basename}'
+    run_prefix = f'{os.path.dirname(config.config_file_path)}.{config_file_basename}'
 
     #check if source data avaible
     source_file_name = config.location + '.csv'
@@ -179,103 +179,6 @@ def incorporate_penalties(dist_df, alpha, run_prefix, result_df, ea_model, confi
 
 
     return result_df
-
-
-    # #build model
-    # ea_model = polling_model_factory(dist_df, alpha, config, exclude_penalized_sites=False)
-    # final_model = ('base model', ea_model)
-
-    # line_number = my_print(f'model built for {run_prefix}.', line_number)
-
-    # #solve model
-    # solve_model(ea_model, config.time_limit, log=log, log_file_path=config.log_file_path)
-    # line_number = my_print(f'model solved for {run_prefix}.', line_number)
-
-    # #incorporate result into main dataframe
-    # result_df = incorporate_result(dist_df, ea_model)
-
-    # selected_sites = set(result_df.id_dest)
-    # penalized_selections = {x for x in selected_sites if x in config.penalized_sites}
-
-    # line_number = my_print(f'Unpenalized model selected {len(penalized_selections)} penalized sites:', line_number)
-    # for s in sorted(penalized_selections):
-    #     line_number = my_print(f'\t ---> {s}', line_number)
-
-    # if penalized_selections: # penalized sites were selected, move to step 3
-    #     obj_value = pyo.value(ea_model.obj)
-    #     kp1 = -1/(config.beta*alpha)*math.log(obj_value) if config.beta else obj_value
-            
-
-    #     # solve model with penalized sites excluded
-    #     ea_model_exclusions = polling_model_factory(dist_df, alpha, config, exclude_penalized_sites=True)
-    #     line_number = my_print(f'model with exclusions built for {run_prefix}.', line_number)
-
-    #     #solve model
-    #     solve_model(ea_model_exclusions, config.time_limit, log=log, log_file_path=config.log_file_path)
-    #     line_number = my_print(f'model with exclusions solved for {run_prefix}.', line_number)
-        
-    #     obj_value = pyo.value(ea_model_exclusions.obj)
-    #     kp2 = -1/(config.beta*alpha)*math.log(obj_value) if config.beta else obj_value
-
-    #     penalty = (kp2-kp1)/len(penalized_selections)
-
-    #     line_number = my_print(f'{kp1 = :.2f}, {kp2 = :.2f}', line_number)
-    #     line_number = my_print(f'computed penalty is {penalty:.2f}', line_number)
-
-    #     ea_model_penalized =  polling_model_factory(dist_df, alpha, config,
-    #                                                 exclude_penalized_sites=False,
-    #                                                 site_penalty=penalty,
-    #                                                 kp_penalty_parameter=kp1)
-    #     final_model = ('penalized', ea_model_penalized)
-    #     line_number = my_print(f'penalized model built for {run_prefix}.', line_number)
-
-    #     #solve model
-    #     solve_model(ea_model_penalized, config.time_limit, log=log, log_file_path=config.log_file_path)
-    #     line_number = my_print(f'penalized model solved for {run_prefix}.', line_number)
-
-    #     #incorporate result into main dataframe
-    #     result_df = incorporate_result(dist_df, ea_model_penalized)
-        
-    #     selected_sites = set(result_df.id_dest)
-    #     penalized_selections = {x for x in selected_sites if x in config.penalized_sites}
-
-    #     if penalized_selections:
-    #         line_number = my_print(f'Penalized model selected {len(penalized_selections)} penalized sites:', line_number)
-    #         for s in sorted(penalized_selections):
-    #             line_number = my_print(f'\t ---> {s}', line_number)
-    #     else:
-    #         line_number = my_print('Penalized model selected no penalized sites.', line_number)
-
-
-    # obj_value = pyo.value(final_model[1].obj)
-    # kp_pen = -1/(config.beta*alpha)*math.log(obj_value) if config.beta else obj_value
-    # kp = compute_kp_score(result_df, config.beta, alpha=alpha)
-    # line_number = my_print(f'Final Model = {final_model[0]}, KP Pen = {kp_pen:.2f}, KP = {kp:.2f}, Penalty={kp_pen-kp:.2f}', line_number)
-
-    # #calculate the new alpha given this assignment
-    # alpha_new = alpha_min(result_df)
-
-    # #calculate the average distances traveled by each demographic to the assigned precinct
-    # demographic_prec = demographic_domain_summary(result_df, 'id_dest')
-
-    # #calculate the average distances traveled by each demographic by residence
-    # demographic_res = demographic_domain_summary(result_df, 'id_orig')
-
-    # #calculate the average distances (and y_ede if beta !=0) traveled by each demographic
-    # demographic_ede = demographic_summary(demographic_res, result_df, config.beta, alpha_new)
-
-    # result_folder = config.result_folder
-
-    # write_results(
-    #     result_folder,
-    #     run_prefix,
-    #     result_df,
-    #     demographic_prec,
-    #     demographic_res,
-    #     demographic_ede,
-    # )
-
-    # return result_folder
 
 def get_log_path(config: PollingModelConfig, specifier: str):
     base_path = '.'.join(config.log_file_path.split('.')[0:-1])
