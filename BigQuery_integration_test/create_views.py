f5181d36566d20f127d2a1e6a49de79fdb05c91a
Ben Mainwaring <mainwaringb@gmail.com>
2024-07-20 18:37:42 -0700

    Functions to read BigQuery data for analysis using R

---
+++ BigQuery_integration_test/create_views.py
@@ -0,0 +1,32 @@
+from google.cloud import bigquery
+
+project = "voting-rights-storage-test"
+dataset = "polling"
+tables = ["result", "edes", "precinct_distances", "residence_distances"]
+
+client = bigquery.Client()
+
+for table in tables:
+
+	view_id = project + "." + dataset + "." + table + "_extra"
+	source_id = project + "." + dataset + "." + table 
+	view = bigquery.Table(view_id)
+
+	# The source table in this example is created from a CSV file in Google
+	# Cloud Storage located at
+	# `gs://cloud-samples-data/bigquery/us-states/us-states.csv`. It contains
+	# 50 US states, while the view returns only those states with names
+	# starting with the letter 'W'.
+	view.view_query = f'''
+	SELECT 
+	  t.*,
+	  c.location,
+	  c.year,
+	  c.precincts_open
+	FROM polling.{table} t LEFT JOIN polling.configs c
+	  ON t.config_name = c.config_name AND t.config_set = c.config_set
+	'''
+
+	# Make an API request to create the view.
+	view = client.create_table(view)
+	print(f"Created {view.table_type}: {str(view.reference)}")
