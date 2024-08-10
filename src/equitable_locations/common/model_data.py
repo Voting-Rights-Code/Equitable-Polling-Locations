fcabec03f4b32dc4495954469d2b39ab0833b2cc
Jason Krizan <34923517+orthorhombic@users.noreply.github.com>
2024-08-10 17:40:24 -0400

    incorporation and edits to initial code for model building

---
+++ src/equitable_locations/common/model_data.py
@@ -0,0 +1,117 @@
+#######################################
+# Created on 6 December 2023
+#
+# @author: Voting Rights Code
+# @attribution: based off of code by Josh Murell
+#######################################
+
+import pandas as pd
+import math
+import os
+from equitable_locations.io.model_config import PollingModelConfig
+
+
+def clean_data(df: pd.DataFrame, config: PollingModelConfig, for_alpha: bool):
+    year_list = config.year
+
+    # pull out unique location types is this data
+    unique_location_types = df["location_type"].unique()
+
+    if for_alpha:
+        bad_location_list = [
+            location_type
+            for location_type in unique_location_types
+            if "Potential" in location_type or "centroid" in location_type
+        ]
+    else:
+        bad_location_list = config.bad_types
+
+    polling_location_types = set(df[df.dest_type == "polling"]["location_type"])
+    for year in year_list:
+        if not any(str(year) in poll for poll in polling_location_types):
+            raise ValueError(f"Do not currently have any data for {config.county_name}, {config.state_name} for {year}")
+    # drop duplicates and empty block groups
+    df = df.drop_duplicates()  # put in to avoid duplications down the line.
+    df = df[df["population"] > 0]
+
+    # exclude bad location types
+
+    # The bad types must be valid location types
+    if not set(bad_location_list).issubset(set(unique_location_types)):
+        raise ValueError(
+            f"unrecognized bad location types types {set(bad_location_list).difference(set(unique_location_types))}"
+        )
+    # drop rows of bad location types in df
+    df = df[~df["location_type"].isin(bad_location_list)]
+
+    # select data based on year
+    # select the polling locations only for the indicated years
+    # keep all other locations
+    not_polling = df[(df.dest_type != "polling")]
+    polling_year_list = [df[df.location_type.str.contains(str(year))] for year in year_list]
+    polling_year_list.append(not_polling)
+    df = pd.concat(polling_year_list)
+    # the concatenation will create duplicates if a polling location is used multiple years
+    # drop these
+    df = df.drop_duplicates()
+
+    # check that population is unique by id_orig
+    pop_df = df.groupby("id_orig")["population"].agg("unique").str.len()
+    if any(pop_df > 1):
+        raise ValueError("Some id_orig has multiple associated populations")
+
+    return df
+
+
+##########################
+# Other functions for data processing
+##########################
+
+
+# determines the maximum of the minimum distances
+def get_max_min_dist(dist_df):
+    min_dist = dist_df[["id_orig", "distance"]].groupby("id_orig").agg("min")
+    max_min_dist = min_dist.distance.max()
+    max_min_dist = math.ceil(max_min_dist)
+    return max_min_dist
+
+
+# various alpha function. Really only use alpha_min
+def alpha_all(df):
+    # add a distance square column
+    df["distance_squared"] = df["distance"] * df["distance"]
+
+    # population weighted distances
+    distance_sum = sum(df["population"] * df["distance"])
+    # population weighted distance squared
+    distance_sq_sum = sum(df["population"] * df["distance_squared"])
+    alpha = distance_sum / distance_sq_sum
+    return alpha
+
+
+def alpha_min(df):
+    # Find the minimal distance to polling location
+    min_df = df[["id_orig", "distance", "population"]].groupby("id_orig").agg("min")
+
+    # find the square of the min distances
+    min_df["distance_squared"] = min_df["distance"] * min_df["distance"]
+    # population weighted distances
+    distance_sum = sum(min_df["population"] * min_df["distance"])
+    # population weighted distance squared
+    distance_sq_sum = sum(min_df["population"] * min_df["distance_squared"])
+    alpha = distance_sum / distance_sq_sum
+    return alpha
+
+
+def alpha_mean(df):
+    # Find the mean distance to polling location
+    mean_df = df[["id_orig", "distance", "population"]].groupby("id_orig").agg("mean")
+
+    # find the square of the min distances
+    mean_df["distance_squared"] = mean_df["distance"] * mean_df["distance"]
+    # population weighted distances
+    distance_sum = sum(mean_df["population"] * mean_df["distance"])
+    # population weighted distance squared
+    distance_sq_sum = sum(mean_df["population"] * mean_df["distance_squared"])
+    alpha = distance_sum / distance_sq_sum
+    return alpha
---
+++ src/equitable_locations/io/model_config.py
@@ -0,0 +1,71 @@
+from typing import List
+from dataclasses import dataclass, field
+import yaml
+from equitable_locations import PROJECT_ROOT
+
+
+@dataclass
+class PollingModelConfig:
+    """A simple config class to run models."""
+
+    state_name: str
+    """Name of the state of interest"""
+    county_name: str
+    """Name of the county of interest"""
+    year: List[str]
+    """list of years to be studied"""
+    bad_types: List[str]
+    """list of location types not to be considered in this model"""
+    beta: float
+    """level of inequality aversion: [-10,0], where 0 indicates indifference, and thus uses the
+    mean. -2 isa good number """
+    time_limit: int
+    """How long the solver should try to find a solution"""
+    penalized_sites: List[str] = field(default_factory=list)
+    """A list of locations for which the preference is to only place a polling location there
+    if absolutely necessary for coverage, e.g. fire stations."""
+
+    precincts_open: int = None
+    """The total number of precincts to be used this year. If no
+    user input is given, this is calculated to be the number of
+    polling places identified in the data."""
+    maxpctnew: float = 1.0
+    """The percent on new polling places (not already defined as a
+    polling location) permitted in the data. Default = 1. I.e. can replace all existing locations"""
+    minpctold: float = 0
+    """The minimun number of polling places (those already defined as a
+    polling location) permitted in the data. Default = 0. I.e. can replace all existing locations"""
+    max_min_mult: float = 1.0
+    """A multiplicative factor for the min_max distance caluclated
+    from the data. Should be >= 1. Default = 1."""
+    capacity: float = 1.0
+    """A multiplicative factor for calculating the capacity constraint. Should be >= 1.
+    Default = 1."""
+    result_folder: str = None
+    """ The location to write out results """
+
+    config_file_path: str = None
+    """ The path to the file that defines this config.  """
+
+    log_file_path: str = None
+    """ If specified, the location of the file to write logs to """
+
+    driving: bool = False
+    """ Driving distances used if True and distance file exists in correct location """
+
+    def __post_init__(self):
+        if not self.result_folder:
+            state_folder = self.state_name.replace(" ", "_")
+            county_folder = self.county_name.replace(" ", "_")
+
+            self.result_folder = PROJECT_ROOT / "untracked" / "results" / state_folder / county_folder
+
+    @staticmethod
+    def load_config(config_yaml_path: str) -> "PollingModelConfig":
+        """Return an instance of RunConfig from a yaml file."""
+        with open(config_yaml_path, "r", encoding="utf-8") as yaml_file:
+            # use safe_load instead load
+            config = yaml.safe_load(yaml_file)
+            result = PollingModelConfig(**config)
+            result.config_file_path = config_yaml_path
+            return result
