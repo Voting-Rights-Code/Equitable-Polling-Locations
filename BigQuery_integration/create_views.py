78ae884d23c25279d86ef5b599eda436689b97be
Ben Mainwaring <mainwaringb@gmail.com>
2024-09-10 20:18:16 -0700

    Cleanup for merge

--- BigQuery_integration/create_views.py
+++ BigQuery_integration/create_views.py
@@ -12,11 +12,6 @@ for table in tables:
 	source_id = project + "." + dataset + "." + table 
 	view = bigquery.Table(view_id)
 
-	# The source table in this example is created from a CSV file in Google
-	# Cloud Storage located at
-	# `gs://cloud-samples-data/bigquery/us-states/us-states.csv`. It contains
-	# 50 US states, while the view returns only those states with names
-	# starting with the letter 'W'.
 	view.view_query = f'''
 	SELECT 
 	  t.*,
