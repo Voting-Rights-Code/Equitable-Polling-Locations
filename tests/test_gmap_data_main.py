6d9fc25952ffaa02f0527658398fc3dd8dbad18f
= <jim.locascio.mlengineer@gmail.com>
2024-10-11 10:35:39 -0400

    add gmap data for Fairfax County VA, upd unit test

--- tests/test_gmap_data_main.py
+++ tests/test_gmap_data_main.py
@@ -1,7 +1,9 @@
 import gmap_data_main
 import pandas as pd
+from authentication_files.file_path import gmap_csv
 
-rtn_val = pd.read_csv("gmap.csv")
+
+rtn_val = pd.read_csv(gmap_csv)
 
 def test_main(mocker):
     mocker.patch('gmap_data_main.add_GMAP_distance_m', return_value=rtn_val) 
