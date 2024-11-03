4d1c21e7f17bce3230812262c028454470cec413
Susama Agarwala <susama.agarwala@covidalliance.org>
2024-11-03 18:55:25 -0500

    County added to file and folder names

--- rename_counties.sh
+++ rename_counties.sh
@@ -8,38 +8,50 @@ declare -a add_county=("Gwinnett_GA" "DeKalb_GA" "Cobb_GA" "Berkeley_SC" "Lexing
 #list of locations where "County" needs to be added
 declare -a change_city=("Norfolk_City_VA" "Virginia_Beach_City_VA" )
 
-for entry in "${add_county[@]}"
+for entry in "${change_city[@]}"; do
   #all but last 3 (_ST)
   County=${entry%???}
   #Just last two (ST)
   State=${entry:(-2)}
-  bad_name="$entry"
-  good_name="$County\_County_$State"
+  bad_name=$entry
+  good_name=${bad_name/'City'/'city'}
   echo "$bad_name -> $good_name"
   ### need to do this twice to get all layers, given current structure
-  bad_name_regexp="\*$bad_name\*"
-  for file_name in $(find . -depth -name $bad_name_regexp ); do
-    mv "$file_name" "${file_name/$bad_name/$good_name}"
+  for file_name in $(find . -depth -name "*$bad_name*"); do
+    temp_file=${file_name/$bad_name/"temp"} 
+    new_file=${file_name/"temp"/$good_name}
+    echo "Temp file name = $temp_file"
+    echo "New file name = $new_file"
+    mv $file_name $temp_file
+    mv $temp_file $new_file
   done
 
-  for file_name in $(find . -depth -name $good_name_regexp ); do
-    mv "$file_name" "${file_name/$bad_name/$good_name}"
+  echo "loop down a directory"
+  for file_name in $(find . -depth -name "*$bad_name*"); do
+    new_file=${file_name/$bad_name/$good_name}
+    echo "New file name = $foo"
+    mv "$file_name" "$new_file"
   done
+done
 
-for entry in "${change_city[@]}"
+
+
+for entry in "${add_county[@]}"; do
   #all but last 3 (_ST)
   County=${entry%???}
   #Just last two (ST)
   State=${entry:(-2)}
-  bad_name="$entry"
-  good_name="${bad_name/City/city}"
+  bad_name=$entry
+  good_name=$County"_County_"$State
   echo "$bad_name -> $good_name"
   ### need to do this twice to get all layers, given current structure
-  bad_name_regexp="\*$bad_name\*"
-  for file_name in $(find . -depth -name $bad_name_regexp ); do
+  for file_name in $(find . -depth -name "*$bad_name*"); do
     mv "$file_name" "${file_name/$bad_name/$good_name}"
   done
 
-  for file_name in $(find . -depth -name '*Cobb_GA*' ); do
+  echo "loop down a directory"
+  for file_name in $(find . -depth -name "*$bad_name*"); do
     mv "$file_name" "${file_name/$bad_name/$good_name}"
   done
+done
+
