4be0e825e5f33448e88466ee3b5f3df04fa2a0ea
Susama Agarwala <susama.agarwala@covidalliance.org>
2024-11-03 21:00:53 -0500

    City to city done by hand

--- rename_counties.sh
+++ rename_counties.sh
@@ -8,33 +8,10 @@ declare -a add_county=("Gwinnett_GA" "DeKalb_GA" "Cobb_GA" "Berkeley_SC" "Lexing
 #list of locations where "County" needs to be added
 declare -a change_city=("Norfolk_City_VA" "Virginia_Beach_City_VA" )
 
-for entry in "${change_city[@]}"; do
-  #all but last 3 (_ST)
-  County=${entry%???}
-  #Just last two (ST)
-  State=${entry:(-2)}
-  bad_name=$entry
-  good_name=${bad_name/'City'/'city'}
-  echo "$bad_name -> $good_name"
-  ### need to do this twice to get all layers, given current structure
-  for file_name in $(find . -depth -name "*$bad_name*"); do
-    temp_file=${file_name/$bad_name/"temp"} 
-    new_file=${file_name/"temp"/$good_name}
-    echo "Temp file name = $temp_file"
-    echo "New file name = $new_file"
-    mv $file_name $temp_file
-    mv $temp_file $new_file
-  done
 
-  echo "loop down a directory"
-  for file_name in $(find . -depth -name "*$bad_name*"); do
-    new_file=${file_name/$bad_name/$good_name}
-    echo "New file name = $foo"
-    mv "$file_name" "$new_file"
-  done
-done
-
-
+#change files manually in ., census/* and result_analysis
+#for the City -> city change
+#otherwise it doesn't seen the case change
 
 for entry in "${add_county[@]}"; do
   #all but last 3 (_ST)
