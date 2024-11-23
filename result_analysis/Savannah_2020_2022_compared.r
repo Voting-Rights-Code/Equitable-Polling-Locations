0bcd008190ab75edd124bd9da2dcaa395e39bcb5
Amasus <38544826+Amasus@users.noreply.github.com>
2024-11-23 17:46:21 -0600

    Feature/analysis from db (#60)
    * initial testing of R database
    
    * configs now read from database
    
    * more refactoring
    
    * basic analysis done
    
    * Madison_2024_compared fixed
    
    * Madison_2024 refactored
    
    * all analysis files updated
    
    * forgot something
    
    * Fixed a merge error
    
    ---------
    
    Co-authored-by: Susama Agarwala <susama.agarwala@covidalliance.org>
    Co-authored-by: Chad Rosenberg <abditus@gmail.com>

--- result_analysis/Savannah_2020_2022_compared.r
+++ result_analysis/Savannah_2020_2022_compared.r
@@ -20,54 +20,67 @@ source('result_analysis/map_functions.R')
 #LOCATION must be either a string or list of strings
 #CONFIG_FOLDER must be a string
 
-LOCATION = c('Contained_in_Savannah_City_of_GA', 'Intersecting_Savannah_City_of_GA')
+LOCATION = c('Contained_in_Savannah_City_of_GA', 'Intersecting_Savannah_City_of_GA') #needed only for reading from csv and writing outputs
 CONTAINED_IN_ORIG_CONFIG_FOLDER = 'Contained_in_Savannah_City_of_GA_original_configs'
 INTERSECTING_ORIG_CONFIG_FOLDER = 'Intersecting_Savannah_City_of_GA_original_configs'
 
 CONTAINED_IN_POT_CONFIG_FOLDER = 'Contained_in_Savannah_City_of_GA_no_bg_school_configs'
 INTERSECTING_POT_CONFIG_FOLDER = 'Intersecting_Savannah_City_of_GA_no_bg_school_configs'
 
-COMPARISON_FOLDER = 'Compare_Savannah_City_of_GA_configs'
+COMPARISON_FOLDER = 'Compare_Savannah_City_of_GA_configs' #only for putting files away
+
+#constants for reading data
+READ_FROM_CSV = TRUE
+TABLES = c("edes", "precinct_distances", "residence_distances", "result")
+
+#constants for database queries
+#only need to define if READ_FROM_CSV = TRUE
+PROJECT = "equitable-polling-locations"
+DATASET = "polling"
+
+#Connect to database if needed
+#returns NULL if READ_FROM_CSV = TRUE
+POLLING_CON <- define_connection()
+
 #######
 #Check that location and folders valid
 #this also ensures that you are in the right folder to read data
 #######
 
-#Do the config folders exist?
-check_config_folder_valid(CONTAINED_IN_ORIG_CONFIG_FOLDER)
-check_config_folder_valid(INTERSECTING_ORIG_CONFIG_FOLDER)
-check_config_folder_valid(CONTAINED_IN_POT_CONFIG_FOLDER)
-check_config_folder_valid(INTERSECTING_POT_CONFIG_FOLDER)
+#Load config data
+#checking if the config folder is valid
+#and that the location is in the indicated dataset
 
+contained_in_orig_config_dt <- load_config_data(LOCATION[1], CONTAINED_IN_ORIG_CONFIG_FOLDER)
+intersecting_orig_config_dt <- load_config_data(LOCATION[2], INTERSECTING_ORIG_CONFIG_FOLDER)
 
-#Does the config folder contain files associated to the location
-check_location_valid(LOCATION[1], CONTAINED_IN_ORIG_CONFIG_FOLDER)
-check_location_valid(LOCATION[2], INTERSECTING_ORIG_CONFIG_FOLDER)
-check_location_valid(LOCATION[1], CONTAINED_IN_POT_CONFIG_FOLDER)
-check_location_valid(LOCATION[2], INTERSECTING_POT_CONFIG_FOLDER)
+contained_in_pot_config_dt <- load_config_data(LOCATION[1], CONTAINED_IN_POT_CONFIG_FOLDER)
+intersecting_pot_config_dt <- load_config_data(LOCATION[2], INTERSECTING_POT_CONFIG_FOLDER)
+
+#get driving flags
+config_dt_list<-c(contained_in_orig_config_dt, intersecting_orig_config_dt, contained_in_pot_config_dt, intersecting_pot_config_dt)
+DRIVING_FLAG <- set_global_driving_flag(config_dt_list)
 
 #######
 #Read in data
 #Run this for each of the folders under consideration
 #Recall, output of form: list(ede_df, precinct_df, residence_df, result_df)
 #######
-#driving flags
-config_list<-c(CONTAINED_IN_ORIG_CONFIG_FOLDER, INTERSECTING_ORIG_CONFIG_FOLDER,    CONTAINED_IN_POT_CONFIG_FOLDER, INTERSECTING_POT_CONFIG_FOLDER)
-DRIVING_FLAG <- set_global_driving_flag(config_list)
 
+#names of the output data in these lists 
+#come from TABLES above
+contained_in_orig_output_df_list <- read_result_data(contained_in_orig_config_dt)
+intersecting_orig_output_df_list <- read_result_data(intersecting_orig_config_dt)
 
-contained_in_orig_df_list <- read_result_data(LOCATION[1], CONTAINED_IN_ORIG_CONFIG_FOLDER)
-intersecting_orig_df_list <- read_result_data(LOCATION[2], INTERSECTING_ORIG_CONFIG_FOLDER)
-
-contained_in_pot_df_list <- read_result_data(LOCATION[1], CONTAINED_IN_POT_CONFIG_FOLDER)
-intersecting_pot_df_list <- read_result_data(LOCATION[2], INTERSECTING_POT_CONFIG_FOLDER)
+contained_in_pot_output_df_list <- read_result_data(contained_in_pot_config_dt)
+intersecting_pot_output_df_list <- read_result_data(intersecting_pot_config_dt)
 
 #change descriptor
-contained_in_orig_df_list = lapply(contained_in_orig_df_list, function(x){x[, descriptor:= str_replace(descriptor, 'year', 'Contained')]})
-intersecting_orig_df_list = lapply(intersecting_orig_df_list, function(x){x[, descriptor:= str_replace(descriptor, 'year', 'Intersecting')]})
+contained_in_orig_output_df_list = lapply(contained_in_orig_output_df_list, function(x){x[, descriptor:= str_replace(descriptor, 'year', 'Contained')]})
+intersecting_orig_output_df_list = lapply(intersecting_orig_output_df_list, function(x){x[, descriptor:= str_replace(descriptor, 'year', 'Intersecting')]})
 
-contained_in_pot_df_list <- lapply(contained_in_pot_config_df_list, function(x) {x[ , descriptor := 'Contained']})
-intersecting_pot_df_list <- lapply(intersecting_pot_config_df_list, function(x) {x[ , descriptor := 'Intersecting']})
+contained_in_pot_output_df_list <- lapply(contained_in_pot_output_df_list, function(x) {x[ , descriptor := 'Contained']})
+intersecting_pot_output_df_list <- lapply(intersecting_pot_output_df_list, function(x) {x[ , descriptor := 'Intersecting']})
 
 #########
 #Set up maps and cartograms
@@ -76,55 +89,49 @@ intersecting_pot_df_list <- lapply(intersecting_pot_config_df_list, function(x) 
 result_folder = paste(LOCATION, 'results', sep = '_')
 
 #get all file names the result_folder with the strings config_folder and 'residence_distances'
-res_dist_list = list.files(result_folder)[grepl('residence_distances', list.files(result_folder))]
-orig_res_dist_list = res_dist_list[grepl(CONTAINED_IN_ORIG_CONFIG_FOLDER, res_dist_list)|grepl(INTERSECTING_ORIG_CONFIG_FOLDER, res_dist_list)]
+contained_in_orig_list_prepped <- prepare_outputs_for_maps(contained_in_orig_output_df_list$residence_distances, contained_in_orig_output_df_list$result, contained_in_orig_config_dt)
+
+intersecting_orig_list_prepped <- prepare_outputs_for_maps(intersecting_orig_output_df_list$residence_distances, intersecting_orig_output_df_list$result, intersecting_orig_config_dt)
+
 
 #get avg distance bounds for map coloring
-contained_color_bounds <- distance_bounds(LOCATION[1], CONTAINED_IN_ORIG_CONFIG_FOLDER)
-intersecting_color_bounds <- distance_bounds(LOCATION[2], INTERSECTING_ORIG_CONFIG_FOLDER)
-global_min <- min(contained_color_bounds[[1]], intersecting_color_bounds[[1]])
-global_max <- max(contained_color_bounds[[2]], intersecting_color_bounds[[2]])
-color_bounds <- list(global_min, global_max)
+all_res_output <- do.call(rbind, c(contained_in_orig_list_prepped, intersecting_orig_list_prepped))
 
+global_color_bounds <- distance_bounds(all_res_output)
 
 #######
 #Plot data
 #######
 
-plot_folder = paste0('result_analysis/', COMPARISON_FOLDER)
-if (file.exists(file.path(here(), plot_folder))){
-    setwd(file.path(here(), plot_folder))
-} else{
+plot_folder = paste0('result analysis/', COMPARISON_FOLDER)
+if (!file.exists(file.path(here(), plot_folder))){
     dir.create(file.path(here(), plot_folder))
-    setwd(file.path(here(), plot_folder))
 }
+setwd(file.path(here(), plot_folder))
 
 ###graphs####
 
 #Add percent population to data ede data for graph scaling for all general config folder and orig
-contained_in_pop_scaled_edes <- ede_with_pop(contained_in_orig_df_list)
-intersecting_pop_scaled_edes <- ede_with_pop(intersecting_orig_df_list)
+contained_in_pop_scaled_edes <- ede_with_pop(contained_in_orig_output_df_list)
+intersecting_pop_scaled_edes <- ede_with_pop(intersecting_orig_output_df_list)
 
 #Plot the edes for all runs in original_location and equivalent optimization runs by demographic
 pop_scaled_list = list(contained_in_pop_scaled_edes, intersecting_pop_scaled_edes)
 combined_pop_scaled_edes <- combine_different_runs(pop_scaled_list)
 plot_historic_edes(combined_pop_scaled_edes, suffix = 'pop_scaled')
 
-edes_to_compare <- list(contained_in_pot_df_list[[1]], intersecting_pot_df_list[[1]])
+edes_to_compare <- list(contained_in_pot_output_df_list$edes, intersecting_pot_output_df_list$edes)
 plot_multiple_edes(edes_to_compare, 'asian')
 plot_multiple_edes(edes_to_compare, 'population')
 plot_multiple_edes(edes_to_compare, 'black')
 plot_multiple_edes(edes_to_compare, 'white')
 
 ###maps####
-make_bg_maps(orig_res_dist_list[1], 'boundries', result_folder_name = result_folder[1], this_location = LOCATION[1])
-make_bg_maps(orig_res_dist_list[2], 'boundries', result_folder_name = result_folder[1], this_location = LOCATION[1])
-make_bg_maps(orig_res_dist_list[3], 'map', result_folder_name = result_folder[2], this_location = LOCATION[2])
-make_bg_maps(orig_res_dist_list[4], 'map', result_folder_name = result_folder[2], this_location = LOCATION[2])
+sapply(contained_in_orig_list_prepped, function(x){make_bg_maps(x, 'boundries', result_folder_name = result_folder[1])})
+sapply(intersecting_orig_list_prepped, function(x){make_bg_maps(x, 'boundries', result_folder_name = result_folder[2])})
 
-make_demo_dist_map(orig_res_dist_list[1], 'population', map_type = 'boundries', result_folder_name =  result_folder[1], this_location = LOCATION[1])
-make_demo_dist_map(orig_res_dist_list[2], 'population', map_type = 'boundries', result_folder_name = result_folder[1], this_location = LOCATION[1])
-make_demo_dist_map(orig_res_dist_list[3], 'population', map_type = 'map', result_folder_name =  result_folder[2], this_location = LOCATION[2])
-make_demo_dist_map(orig_res_dist_list[4], 'population', map_type = 'map', result_folder_name =  result_folder[2], this_location = LOCATION[2])
+sapply(contained_in_orig_list_prepped, function(x){make_demo_dist_map(x, 'population', map_type = 'boundries', result_folder_name =  result_folder[1])})
+sapply(intersecting_orig_list_prepped, function(x){make_demo_dist_map(x, 'population', map_type = 'boundries', result_folder_name =  result_folder[2])})
+
 
 
--- result_analysis/map_functions.R
+++ result_analysis/map_functions.R
@@ -26,7 +26,7 @@ library(cartogram)
 #	 * These two get merged and written to file
 # 2. make_bg_maps:
 #	 * takes base maps of cartograms and associates average distance traveled to each block group
-# 	 * process_residence:
+# 	 * aggregate_residence:
 #	 	* takes the output of the optimization process and aggregates average distance up to the block group level
 #	 	* the aggregation is done by removing the last three digits of the block group id
 #	 	* returns only the distance data for a specific demographic group
@@ -41,10 +41,43 @@ library(cartogram)
 #	 * the size of the dot corresponds to population. 
 # 	 	* the size scale is determined by the total populations of the block groups
 
-source('result_analysis/graph_functions.R')
+add_location <- function(data, config_dt){
+	#adds location field from config_dt to output data
+	
+	#get config meta data and locations
+	config_set_location <- config_dt[ , .(location, config_set, config_name)]
+
+	#merge
+	data_with_loc <- merge(data, config_set_location, by = c('config_set', 'config_name'))
+
+	return(data_with_loc)
+}
+
+distance_bounds <- function(residence_df){
+	#calculate the min and max average distances traveled by census block for maps by location
+
+	#select total population residence data
+	pop_res_dist <- residence_df[demographic == 'population', ]
+	
+	# pull out min and max average distances by location
+	bound_dt <- pop_res_dist[, .(min_avg_dist = min(avg_dist), max_avg_dist = max(avg_dist)), by = location]
+
+	# check for missing data
+	if (any(sapply(bound_dt, anyNA))){
+    warning('Some location does not have a min or max average distance for mapping')}
+
+	# pull out global bounds
+	global_max <- max(bound_dt$max_avg_dist)
+	global_min <- max(bound_dt$min_avg_dist)
+	color_bounds <- list(global_min, global_max)
+	return(color_bounds)
+}
 
 
 process_maps <- function(file_name){
+	#reads in map data, ads an area column and returns only 
+	#polygon id (GEOID20), the area, lat, lon and polygon geometry
+
     #read in map data as data table
     map <-st_read(file_name)
     map_dt <- as.data.table(map)
@@ -80,49 +113,55 @@ process_demographics <-function(folder_name){
 	demo = demo[population >0, ]
 }
 
-process_residence <- function(file_name, demo_str, result_folder){
-	#read in residence data from the model output, which is at the block 
-	#level, and aggregate it at the block group level,
-	#selecting for the given demographic
+aggregate_residence_demo <- function(res_dist_df){
+	#aggregate residence data from the model output, which is at the block 
+	#level, to the block group level
 
-	#read in residence data set
-	if (!grepl('residence_distances', file_name)){
-		stop('file_name not a residence_distances file')
-	}
-	#read in data
-	res_dist_df <- fread(paste0(here(), '/',result_folder, '/', file_name))
-
-	#the last 3 digits are the block, remove these for block group
+	#the last 3 digits of id_orig are the block, remove these for block group
 	#GEOID20 is the BG column name from the census
 	res_dist_df <- res_dist_df[ , id_orig := as.character(id_orig)
 		][ , GEOID20 := gsub('.{3}$', '', id_orig)]
 	
-	#aggregate by block group
+	#aggregate by block group, demographic and config_name
 	bg_res_dist <- res_dist_df[ , .(demo_pop = sum(demo_pop),weighted_dist = sum(weighted_dist)), 
-										by = c('GEOID20', 'demographic')][ , avg_dist := weighted_dist/demo_pop]
+						by = c('GEOID20', 'demographic', 'config_set','config_name', 'descriptor')][ , avg_dist := weighted_dist/demo_pop]
 
-	#select demographic
-	bg_res_demo_dist <- bg_res_dist[demographic == demo_str, ]
-	return(bg_res_demo_dist)
+	return(bg_res_dist)
 }
 
-distance_bounds <- function(location, config_folder, field_of_interest = ''){
-	#calculate the min and max average distances traveled by census block for maps
+prepare_outputs_for_maps <- function(residence_data, result_data, config_data){
 
-	#get full residence data
-	residence_df<- combine_results(location, config_folder, 'residence_distances', field_of_interest)
-	residence_df <- residence_df[demographic == 'population', ]
-	min_avg_dist <- min(residence_df$avg_dist)
-	max_avg_dist <- max(residence_df$avg_dist)
-	return(list(min_avg_dist, max_avg_dist))
+	#aggregate block level demographic data to block group level
+	res_dist_demo <- aggregate_residence_demo(residence_data)
+	
+	#get lat/ lon coords from results
+	dest_lat_lon <- result_data[ ,.(lon = unique(dest_lon), lat = unique(dest_lat), type = unique(dest_type)), by = c('id_dest', 'config_set', 'config_name')]
+
+	#combine with residence data
+	#note cartesian join
+	res_dist_demo_dest <- merge(dest_lat_lon, res_dist_demo, by = c('config_set', 'config_name'), allow.cartesian = TRUE)
+
+	#add location to above
+	combined_data_with_loc <- add_location(res_dist_demo_dest, config_data) 
+
+	#split by config_name
+	data_list <- split(combined_data_with_loc, combined_data_with_loc$config_name)
+	
+	return(data_list)
 }
 
 make_or_load_maps <- function(location, map_type, demographic = 'population'){
 	#checks if the desired map .csv already exists, and load it if it does
 	#else, makes the map by loading the census shape and demographic data
 	#and merging.
+	#1. set a name for a map
+	#2. check if the map exists. if so, load, else
+	#2a. get block group level demographics
+	#2b. get block group shape files
+	#2c. merge shape file with demographics, assign projection
+	#2d. make map and write to file
 
-	#name the desired map
+	#1. name the desired map
 	if (map_type == 'cartogram'){
 		map_name<- paste0(location, "_", map_type, '_', demographic, '.shp')
 	} else if (map_type == 'map'){
@@ -132,37 +171,42 @@ make_or_load_maps <- function(location, map_type, demographic = 'population'){
 	} else{
 		stop('map_type must be either map, boundries or cartogram')
 	}
-	map_folder <- 'result_analysis/map_work'
-	#load if it exists, else make
+	
+	map_folder <- 'result analysis/map work'
+	
+	#2. load if it exists, else make
 	print(map_name)
 	if (file.exists(file.path(here(), map_folder, map_name))){
 		map <- st_read(file.path(here(), map_folder, map_name))
 		#name resetting needed because of st_write truncating names
 		names(map) <- c("GEOID20", "AREA20", "INTPTLAT20", "INTPTLON20", "Geographic Area Name", "population", "white", "black", "native", "asian", "pacific_islander", "other", "multiple_races", "hispanic", "non_hispanic", "geometry")
-	} else {
-		#block group demographics
-		if (map_type == 'boundries') {
+	} else { 
+		#2a. get block group demographics
+		if (map_type == 'boundries') { #associate block group demographics based on included blocks only
+
+		#get block level demographics
 		block_demo <- process_demographics(paste0(here(), '/datasets/census/redistricting/', location))
+		#define columns for aggregation
 		demo_names <- names(block_demo)[sapply(block_demo,is.integer) ==TRUE]		
-		block_demo <- process_demographics(paste0(here(), '/datasets/census/redistricting/', location))
+		#aggregate up to block group level
 		bg_demo <- block_demo[ , Geography := gsub('.{3}$', '', Geography)
 							][ ,`Geographic Area Name` := gsub('^.{12}', '',`Geographic Area Name` )
 							][ , lapply(.SD, sum), by = c('Geography','Geographic Area Name'), .SDcols = demo_names]
-		} else {
+		} else { # get demographics from block groups
 		bg_demo <- process_demographics(paste0(here(), '/datasets/census/redistricting/', location, "/block group demographics"))
 		}
-		#get shape file
+		#2b. get shape file
 		#Block group shape files
-		if (map_type == 'boundries') {
+		if (map_type == 'boundries') { #use the Intersecting data for both maps
 			bg_shape_file <- list.files(paste0(here(), '/datasets/census/tiger/',gsub('Contained_in', 'Intersecting', location) ), pattern = 'bg20.shp$')
-			#get map data
+			#get processed map data
 			map_bg_dt <- process_maps(paste0(here(), '/datasets/census/tiger/', gsub('Contained_in', 'Intersecting', location) , '/', bg_shape_file))
 		} else {
 			bg_shape_file <- list.files(paste0(here(), '/datasets/census/tiger/', location ), pattern = 'bg20.shp$')
-			#get map data
+			#get processed map data
 			map_bg_dt <- process_maps(paste0(here(), '/datasets/census/tiger/', location, '/', bg_shape_file))
 		}
-		#merge the bg shape dt with the bg demo dt
+		#2c. merge the bg shape dt with the bg demo dt
 		bg_demo_shape <- merge(map_bg_dt, bg_demo, by.x = c('GEOID20'), by.y = c('Geography'))
 		#make it an sf object for mapping
 		bg_demo_sf <- st_as_sf(bg_demo_shape)
@@ -172,44 +216,44 @@ make_or_load_maps <- function(location, map_type, demographic = 'population'){
 		} else {
 			projection = 3857 #correct projection for merc
 		}
-		#make map and write to file
+		#2d. make map and write to file
 		map <- st_transform(bg_demo_sf, projection)
 		if (map_type == 'cartogram'){
 			map <- cartogram_cont(map, demographic, itermax = 200, maxSizeError = 1.02)
-			
 		}
 		st_write(map, file.path(here(), map_folder, map_name))
 	}
 	return(map)
 }
 
-make_bg_maps <-function(file_to_map, map_type, result_folder_name = result_folder, this_location = LOCATION, demo_str = 'population', driving_flag = DRIVING_FLAG){
-	#read in a residence_distance file from the correct config_folder, combine this and use it to color the map by distance to matched location, for the indicated demographic
+make_bg_maps <-function(prepped_data, map_type, result_folder_name = result_folder,  demo_str = 'population', driving_flag = DRIVING_FLAG, color_bounds = global_color_bounds){
+	#use residence_distances to color the map by distance to matched location, for the indicated demographic
 	#If the map type is "map", then also plot the polling locations
 
-	#read in block level data and aggregate to block group level
-	res_dist_demo <- process_residence(file_to_map, demo_str, result_folder_name)
+	#get the location from the data
+	location <- unique(prepped_data$location)
+	
+	#select only the desired demographic
+	prepped_data_demo <- prepped_data[demographic == demo_str, ]
 
 	#extract demographics from map
 	#must do this way because map is an sf object, not a data.table
 	if (map_type == 'cartogram'){
-		map_demo = make_or_load_maps(this_location, map_type, demo_str)
+		warning('Using cartograms is deprecated due to difficulties with convergence of the map.')
+		map_demo = make_or_load_maps(location, map_type, demo_str)
 		map_name = paste('distance', demo_str, map_type, sep = '_')
-	}else if (map_type %in% c('map', 'boundries')) { #map_type !=cartogram
-		map_demo = make_or_load_maps(this_location, map_type)
+	} else if (map_type %in% c('map', 'boundries')) { #map_type !=cartogram
+		#note, in this case we make a population level map
+		map_demo = make_or_load_maps(location, map_type)
 		map_name = paste('distance', map_type, sep = '_')
-		#in this case, also put in the precincts
-		ev_df_name <-sub('residence_distances', 'result', file_to_map)
-		result_df <- fread(paste0(here(), '/',result_folder_name, '/', ev_df_name))
-		ev_locs <- result_df[ , .(long = unique(dest_lon), lat = unique(dest_lat), type = unique(dest_type)), by = id_dest]
 	} else {
 		stop('map_type must be either map, cartogram or boundries')
 	}
 
-	#combine with res_dist_demo with map
+	#combine map data with output data, keeping only polygons for which we have distances
 	geom_cols <- c('GEOID20', 'geometry')
 	map_sf <- map_demo[, geom_cols]
-	demo_dist_shape<- merge(map_sf, res_dist_demo, all = T)	
+	demo_dist_shape<- merge(map_sf, prepped_data_demo, all.y = T)
 	
 	#make maps
 	if (driving_flag){
@@ -220,47 +264,43 @@ make_bg_maps <-function(file_to_map, map_type, result_folder_name = result_folde
 		fill_str = 'Avg straight line distance (m)'
 	}
 
-	county = gsub('.{3}$','',this_location)
-	numeric_label = str_extract(file_to_map, '[0-9]+')
-	descriptor = paste(county, numeric_label, sep ='_')
+	county = gsub('.{3}$','', location)
+	descriptor = paste(county, unique(demo_dist_shape$descriptor), sep ='_')
 	plotted <- ggplot() +
 		geom_sf(data = demo_dist_shape, aes(fill = avg_dist)) + 
 		scale_fill_gradient(low='white', high='darkgreen', limits = c(color_bounds[[1]], color_bounds[[2]]), name = fill_str) 
 	if (map_type != 'cartogram'){
 		plotted = plotted + 
-		geom_point(data = ev_locs, aes(x = long, y = lat, color = type))+ 
+		geom_point(data = demo_dist_shape, aes(x = lon, y = lat, color = type))+ 
 		scale_color_manual(breaks = c('polling', 'potential', 'bg_centroid'), values = c('red', 'black', 'dimgrey'), name = 'Poll Type') +  xlab('') + ylab('') 
 	} else{ 
 		plotted = plotted + theme(axis.text.x=element_blank(), axis.text.y=element_blank(), axis.ticks = element_blank())
 	}
-	plotted = plotted + ggtitle(title_str, paste('Block group', map_type , 'of', gsub('_', ' ', descriptor) ))
-
-	if (driving_flag){
-	plotted <- plotted #+ labs(fill = 'Avg driving distance (m)')
-	} else {
-		plotted <- plotted #+ labs(fill = 'Avg straight line distance (m)')
-	}
+	plotted = plotted + ggtitle(title_str, paste('Block group map', 'of', gsub('_', ' ', descriptor) ))
 
 	#write to file
-	descriptor = gsub(".*configs.(.*)_res.*", "\\1", file_to_map)
-	num_polls <- str_extract(descriptor, '[0,-9]+')
 	ggsave(paste0(here(), '/', plot_folder, '/',map_name, '_',descriptor, '_','polls.png'), plotted)
 	}
 
-make_demo_dist_map <-function(file_to_map, demo_str, result_folder_name = result_folder, this_location = LOCATION, map_type = 'map', driving_flag = DRIVING_FLAG){
+make_demo_dist_map <-function(prepped_data, demo_str, map_type = 'map', result_folder_name = result_folder,  driving_flag = DRIVING_FLAG, color_bounds = global_color_bounds){
+	#use demographic residence_distances to put a dot in  the map colored by distance and sized by population 
 
-	#read in block level data and aggregate to block group level
-	res_dist_df <- process_residence(file_to_map, demo_str, result_folder_name)
+	#get the location from the data
+	location <- unique(prepped_data$location)
+	
+	#select only the desired demographic
+	prepped_data_demo <- prepped_data[demographic == demo_str, ]
 	#get map
-	map_demo <- make_or_load_maps(this_location, map_type)
+	map_demo <- make_or_load_maps(location, map_type)
+
+	#combine map data with output data, keeping rows for which we have distances
 	geom_cols <- c('GEOID20', 'INTPTLON20', 'INTPTLAT20', 'geometry')
 	map_sf <- map_demo[, geom_cols]
 	#change lat/lon to numeric
 	map_sf$INTPTLON20 <- as.numeric(map_sf$INTPTLON20)
 	map_sf$INTPTLAT20 <- as.numeric(map_sf$INTPTLAT20)
-
 	#merge
-	demo_dist_shape<- merge(map_sf, res_dist_df, all = T)
+	demo_dist_shape<- merge(map_sf, prepped_data_demo, all.y = T)
 
 	#plot map with a point at the centroid, colored by distance, sized by size
 	#set names
@@ -272,13 +312,12 @@ make_demo_dist_map <-function(file_to_map, demo_str, result_folder_name = result
 		color_str = 'Avg straight line distance (m)'
 	}
 
-	county = gsub('.{3}$','',this_location)
-	numeric_label = str_extract(file_to_map, '[0-9]+')
-	descriptor = paste(county, numeric_label, sep ='_')
+	county = gsub('.{3}$','',location)
+	descriptor = paste(county, unique(demo_dist_shape$descriptor), sep ='_')
+
 	#size limits
-	pop_dist_df <- process_residence(file_to_map, 'population', result_folder_name)
-	max_pop <- max(pop_dist_df$demo_pop)
-	print(max_pop)
+	max_pop <- max(prepped_data$demo_pop)
+
 	plotted <- ggplot() +
 		geom_sf(data = demo_dist_shape) +  
 		geom_point(data = demo_dist_shape, aes(x = INTPTLON20, y = INTPTLAT20, size= demo_pop, color = avg_dist)) +
@@ -288,8 +327,6 @@ make_demo_dist_map <-function(file_to_map, demo_str, result_folder_name = result
 		ggtitle(paste(demographic_legend_dict[demo_str], title_str), paste('Block groups in', gsub('_', ' ', descriptor)))
 
 	#write to file
-	descriptor = gsub(".*configs.(.*)_res.*", "\\1", file_to_map)
-	num_polls <- str_extract(descriptor, '[0,-9]+')
 	ggsave(paste0(here(), '/', plot_folder, '/', demo_str, '_','pop_and_dist','_',descriptor, '_','polls.png'), plotted)
 
 }
