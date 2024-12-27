dcb9c5ca4eef088c3b647514860fbdcde09a847a
Susama Agarwala <susama.agarwala@covidalliance.org>
2024-12-27 17:46:12 -0500

    everything uploaded to db

--- result_analysis/add_V1.r
+++ result_analysis/add_V1.r
@@ -11,12 +11,8 @@ Dekalb_result_files <- Dekalb_files[grepl('result', Dekalb_files)]
 insert_V1 <- function(file_name){
     file_path <- paste0('DeKalb_County_GA_results/', file_name)
     dt <- fread(file_path)
-
-    if (!("V1" %in% names(dt))){
-        setnames(dt, '', 'V1')
-        fwrite(dt, file_path)
-
-    }
+    fwrite(dt, file_path)
+    
     return
 }
 sapply(Dekalb_result_files, function(x)insert_V1(x))
