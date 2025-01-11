1eb9ac95323d67f24c72269d4498c6b62206379e
Susama Agarwala <susama.agarwala@covidalliance.org>
2025-01-11 18:20:50 -0500

    New penalized graphs run

--- result_analysis/Basic_analysis.r
+++ result_analysis/Basic_analysis.r
@@ -22,8 +22,8 @@ source('result_analysis/map_functions.R')
 #CONFIG_FOLDER must be a string
 
 LOCATION = 'DeKalb_County_GA' #needed only for reading from csv and writing outputs
-ORIG_CONFIG_FOLDER = "DeKalb_County_GA_original_configs_log_driving"
-POTENTIAL_CONFIG_FOLDER = "DeKalb_County_GA_no_bg_school_configs_log_driving_pre_EV_2024"
+ORIG_CONFIG_FOLDER = "DeKalb_County_GA_original_configs_driving"
+POTENTIAL_CONFIG_FOLDER = "DeKalb_County_GA_no_school_penalize_bg_configs_driving_pre_EV_2024"
 ORIG_FIELD_OF_INTEREST = '' #must not leave empty if config set has only one element
 POTENTIAL_FIELD_OF_INTEREST = '' #must not leave empty if config set has only one element
 
@@ -32,8 +32,8 @@ STORAGE_BUCKET = 'equitable-polling-analysis'
 CLOUD_STORAGE_ANALYSIS_NAME = paste0(ORIG_CONFIG_FOLDER, '_AND_', POTENTIAL_CONFIG_FOLDER)
 
 #constants for reading data
-READ_FROM_CSV = FALSE
-PRINT_SQL = TRUE
+READ_FROM_CSV = TRUE
+PRINT_SQL = FALSE
 
 #constants for database queries
 #only need to define if READ_FROM_CSV = TRUE
@@ -51,8 +51,9 @@ POLLING_CON <- define_connection()
 #######
 #refresh google cloud connection
 #######
-system("gcloud auth application-default login")
-
+if (!READ_FROM_CSV){
+    system("gcloud auth application-default login")
+}
 #######
 #Check that location and folders valid
 #Load configs and get driving flags
