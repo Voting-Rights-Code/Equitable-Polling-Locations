df9b6008fe350d1e544e15235a605104c4b07ee7
Susama Agarwala <susama.agarwala@covidalliance.org>
2025-01-18 16:57:16 -0500

    moved some other one off helper functions

--- rename_DeKalb.sh
+++
@@ -1,7 +0,0 @@
-#just change certain DeKalb runs
-for file_name in $(find "./DeKalb_County_GA_results/" "Dekalb_GA*"); do 
-  echo "$file_name"
-#  $bad_name="DeKalb_County_GA"
-#  $good_name="Dekalb_County_GA" 
-  mv "$file_name" "${file_name/"DeKalb_County_GA"/"Dekalb_County_GA" }"
-done
