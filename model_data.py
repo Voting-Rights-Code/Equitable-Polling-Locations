e7cdc2b40cca21a83610f38b6c156e3b92cf9b8f
Susama Agarwala <susama.agarwala@covidalliance.org>
2025-03-13 15:17:37 -0400

    Richmond_County_2024 run

--- model_data.py
+++ model_data.py
@@ -338,8 +338,8 @@ def clean_data(config: PollingModelConfig, for_alpha: bool, log: bool=False):
     df = df[~df['location_type'].isin(bad_location_list)]
 
     #select data based on year
-    #select the polling locations only for the indicated years
-    #keep all other locations
+    #mark everything but bg_centroid as potential
+    #then mark location_types with correct years as 
     df['dest_type'].mask(df['dest_type'] != 'bg_centroid', 'potential', inplace = True)
     df['dest_type'].mask(df['location_type'].str.contains('|'.join(year_list)), 'polling', inplace = True)
     #check that this hasn't created duplicates (should not have); drop these
