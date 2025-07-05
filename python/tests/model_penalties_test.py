1e95883f26b5dc6a96471974e6f7e4125d8ed233
Susama Agarwala <susama.agarwala@covidalliance.org>
2025-07-05 11:56:21 -0400

    everythign passes

--- python/tests/model_penalties_test.py
+++ python/tests/model_penalties_test.py
@@ -12,6 +12,7 @@ from python.solver.model_config import PollingModelConfig
 from python.solver.model_penalties import incorporate_penalties
 from python.solver.model_results import incorporate_result
 from python.solver.model_solver import solve_model
+from python.solver.model_data import clean_data, alpha_min
 
 def test_incorperate_results_and_penalties(testing_config_exclude: PollingModelConfig):
     '''
@@ -57,7 +58,7 @@ def test_incorporate_penalties(result_exclude_df, result_penalized_df, result_ke
     assert result_exclude_kp_factor_sum <= result_penalized_kp_factor_sum
     assert result_penalized_kp_factor_sum <= result_keep_kp_factor_sum
 
-def test_kp1(testing_config_keep, testing_config_penalty, distances_df, alpha_min):
+def test_kp1(testing_config_keep, testing_config_penalty, polling_locations_df):
     #to test that kp1 is correctly defined on line 43
     #Define:
     #  keep_config = test_config_keep.yaml
@@ -69,23 +70,37 @@ def test_kp1(testing_config_keep, testing_config_penalty, distances_df, alpha_mi
     #  kp1 = the value of line 43 when run on penalize_config
     #check that kp1 == keep_obj_value
 
+    #get configs
     keep_config = testing_config_keep
     penalize_config = testing_config_penalty
 
-    keep_model = model_factory.polling_model_factory(distances_df, alpha_min, keep_config)
+    #get distances
+    keep_distances = clean_data(keep_config, polling_locations_df, False, False)
+    penalty_distances = clean_data(penalize_config, polling_locations_df, False, False)
+
+    #get alphas
+    keep_alpha_df = clean_data(keep_config, polling_locations_df, True, False)
+    keep_alpha = alpha_min(keep_alpha_df)
+
+    penalize_alpha_df = clean_data(penalize_config, polling_locations_df, True, False)
+    penalize_alpha = alpha_min(penalize_alpha_df)
+
+    #compute models
+    keep_model = model_factory.polling_model_factory(keep_distances, keep_alpha, keep_config)
     model_solver.solve_model(keep_model, keep_config.time_limit)
     keep_obj_value = pyo.value(keep_model.obj)
 
-    penalize_model = model_factory.polling_model_factory(distances_df, alpha_min, penalize_config)
+    penalize_model = model_factory.polling_model_factory(penalty_distances, penalize_alpha, penalize_config)
     model_solver.solve_model(penalize_model, penalize_config.time_limit)
     penalize_obj_value = pyo.value(penalize_model.obj)
 
-    kp1 = model_penalties.compute_kp(penalize_config, alpha_min, penalize_obj_value)
-    keep_kp = model_penalties.compute_kp(keep_config, alpha_min, keep_obj_value)
+    #get objectives
+    kp1 = model_penalties.compute_kp(penalize_config, penalize_alpha, penalize_obj_value)
+    keep_kp = model_penalties.compute_kp(keep_config, keep_alpha, keep_obj_value)
 
     assert kp1 == keep_kp
 
-def test_kp2(testing_config_exclude, testing_config_penalty, distances_df, alpha_min):
+def test_kp2(testing_config_exclude, testing_config_penalty, polling_locations_df):
     #to test that kp2 is correctly defined on line 57
     #Define:
     #  exclude_config = test_config_exclude.yaml
@@ -97,32 +112,43 @@ def test_kp2(testing_config_exclude, testing_config_penalty, distances_df, alpha
     #  kp2 = the value of line 57 when run on penalize_config
     #check that kp2 == exclude_obj_value
 
+    #get configs
     exclude_config = testing_config_exclude
     penalize_config = testing_config_penalty
 
-    exclude_model = model_factory.polling_model_factory(distances_df, alpha_min, exclude_config)
+    #get distances
+    exclude_distances = clean_data(exclude_config, polling_locations_df, False, False)
+    penalty_distances = clean_data(penalize_config, polling_locations_df, False, False)
+
+    #get alphas
+    exclude_alpha_df = clean_data(exclude_config, polling_locations_df, True, False)
+    exclude_alpha = alpha_min(exclude_alpha_df)
+
+    penalize_alpha_df = clean_data(penalize_config, polling_locations_df, True, False)
+    penalize_alpha = alpha_min(penalize_alpha_df)
+
+    #compute models
+    exclude_model = model_factory.polling_model_factory(exclude_distances, exclude_alpha, exclude_config)
     model_solver.solve_model(exclude_model, exclude_config.time_limit)
     exclude_obj_value = pyo.value(exclude_model.obj)
-    
-    matching_list= [(key[0], key[1], exclude_model.matching[key].value) for key in exclude_model.matching]
+
+    penalize_model = model_factory.polling_model_factory(penalty_distances, penalize_alpha, penalize_config, exclude_penalized_sites=True)
+    model_solver.solve_model(penalize_model, penalize_config.time_limit)
+    penalize_obj_value = pyo.value(penalize_model.obj)
+
+    matching_list= [(key[0], key[1], penalize_model.matching[key].value) for key in penalize_model.matching]
     matching_df = pd.DataFrame(matching_list, columns = ['id_orig', 'id_dest', 'matching'])
 
     #the matching doesn't always give an integer value. Replace the value with the integer it would round to   
     matching_df['matching'].mask(matching_df['matching']>=0.5, 1, inplace=True)
     matching_df['matching'].mask(matching_df['matching']<0.5, 0, inplace=True)
     all_sites = set(matching_df.id_dest)
+    #breakpoint()
 
-    #only matched sites
-    matched_df = matching_df.loc[matching_df['matching'] ==1]
-    selected_sites = set(matched_df.id_dest)
-    breakpoint()
 
-    penalize_model = model_factory.polling_model_factory(distances_df, alpha_min, penalize_config, exclude_penalized_sites=True)
-    model_solver.solve_model(penalize_model, penalize_config.time_limit)
-    penalize_obj_value = pyo.value(penalize_model.obj)
-
-    kp2 = model_penalties.compute_kp(penalize_config, alpha_min, penalize_obj_value)
-    exclude_kp = model_penalties.compute_kp(exclude_config, alpha_min, exclude_obj_value)
+    #get objectives
+    kp2 = model_penalties.compute_kp(penalize_config, penalize_alpha, penalize_obj_value)
+    exclude_kp = model_penalties.compute_kp(exclude_config, exclude_alpha, exclude_obj_value)
 
     assert kp2 == exclude_kp
 
