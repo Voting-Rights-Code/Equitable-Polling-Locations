d926c5dc5f844d209d827f763d718a7c430bc35a
Susama <ganga.faring@gmail.com>
2026-06-20 22:28:22 +0000

    feat(precincts): add extract_precincts.r engine, first step extracts Monongalia's precincts

---
+++ R/result_analysis/Extraction_configs/Monongalia_County_WV.r
@@ -0,0 +1,4 @@
+######## Set constants########
+PRECINCT_SOURCE_FILE <- "datasets/precincts/West_Virginia_25/VotingPrecincts_20260424_wmA84.shp"
+COUNTY_FIPS <- "54061"
+EXPECTED_PRECINCT_COUNT <- 44
---
+++ R/result_analysis/extract_precincts.r
@@ -0,0 +1,40 @@
+library(sf)
+library(here)
+
+setwd(here())
+source("R/result_analysis/utility_functions/shape_extraction_functions.r")
+
+######## Set constants########
+CRS_PROJECTION <- 4326
+
+#######
+# Read in command line arguments
+# A config file must be given to get the location-specific constants for the
+# extraction to be run. To extract a different county or state, add a new
+# config file under R/result_analysis/Extraction_configs/ instead of editing
+# this file. The config's contents will grow as more steps are added to this
+# script.
+#######
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 1) {
+  stop("Must enter exactly one config file")
+} else {
+  config_path <- paste0("R/result_analysis/Extraction_configs/", args[1])
+  source(config_path)
+}
+
+###
+# For inline testing only
+###
+# source("R/result_analysis/Extraction_configs/Monongalia_County_WV.r")
+
+###### Step 1: extract and validate the county's precincts#######
+county_precincts <- extract_county_precincts(PRECINCT_SOURCE_FILE, COUNTY_FIPS, CRS_PROJECTION)
+
+stopifnot(
+  "Precinct count does not match EXPECTED_PRECINCT_COUNT in the config file" =
+    nrow(county_precincts) == EXPECTED_PRECINCT_COUNT
+)
+
+cat(sprintf("Extracted %d precincts into memory.\n", nrow(county_precincts)))
