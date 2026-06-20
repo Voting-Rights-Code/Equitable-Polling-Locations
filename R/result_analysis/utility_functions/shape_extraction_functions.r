c37ce7a4f9909866f4505b70cd866a657be231ba
Susama <ganga.faring@gmail.com>
2026-06-20 22:16:18 +0000

    feat(precincts): add extract_county_precincts to shared shape utils

--- R/result_analysis/utility_functions/shape_extraction_functions.r
+++ R/result_analysis/utility_functions/shape_extraction_functions.r
@@ -16,6 +16,20 @@ get_shape_data <- function(shape_file_path, crs_projection) {
   return(shape_data)
 }
 
+# select a county's precincts from a statewide precinct shapefile,
+# by FIPS code
+extract_county_precincts <- function(precinct_source_file, county_fips,
+                                     crs_projection) {
+  statewide_precincts <- get_shape_data(
+    precinct_source_file,
+    crs_projection
+  )
+  county_precincts <- statewide_precincts[
+    which(statewide_precincts$StCoFIPS == county_fips),
+  ]
+  return(county_precincts)
+}
+
 # select intersecting or contained shape data from a county, given a boundary
 get_shapes_in_boundary <- function(boundary_shape_data, county_shape_data,
                                    intersection_flag) {
