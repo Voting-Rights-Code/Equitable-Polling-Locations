6e74b820a7fb028876e3c19305d3a0837cf95e7c
Amasus <ganga.faring@gmail.com>
2026-07-03 15:01:46 +0000

    error handling to config files

--- R/result_analysis/Extraction_configs/Monongalia_County_WV_no_precinct_data.r
+++ R/result_analysis/Extraction_configs/Monongalia_County_WV_no_precinct_data.r
@@ -9,12 +9,26 @@ STATE_PRECINCT_SOURCE_FILE <- "datasets/precincts/West_Virginia_20260424_wmA84/V
 COUNTY_PRECINCT_SOURCE_FILE <- paste0("datasets/polling/", LOCATION, "/", LOCATION, "_potential_locations.csv")
 
 ########
-# County-provided precinct/polling-location reconciliation (#282)
+# County-provided data and associated labels.
 ########
 COUNTY_PROVIDES_PRECINCT_DATA <- FALSE
-COUNTY_PROVIDED_PRECINCT_FILE <- "temp/Precincts_by_Location.csv"
-COUNTY_PRECINCT_LOCATION_NAME_COL <- "Polling Place Name"
-COUNTY_PRECINCT_ADDRESS_COL <- "Polling Location Address"
+COUNTY_PROVIDED_PRECINCT_FILE <- NULL #cannnot be null if COUNTY_PROVIDES_PRECINCT_DATA is TRUE
+COUNTY_PRECINCT_LOCATION_NAME_COL <- NULL #cannnot be null if COUNTY_PROVIDES_PRECINCT_DATA is TRUE
+COUNTY_PRECINCT_ADDRESS_COL <- NULL #cannnot be null if COUNTY_PROVIDES_PRECINCT_DATA is TRUE
+
+#check that proper file / columns names are added if and only if the county provides data
+county_provided_data_names <- c(COUNTY_PROVIDED_PRECINCT_FILE, COUNTY_PRECINCT_LOCATION_NAME_COL, 
+                                                        COUNTY_PRECINCT_ADDRESS_COL) 
+if (COUNTY_PROVIDES_PRECINCT_DATA) {
+  if (any (is.null(county_provided_data_names))) {
+    stop("COUNTY_PROVIDED_ file, precinct location name column, or precinct address column cannot be null 
+        if COUNTY_PROVIDES_PRECINCT_DATA is TRUE")
+  }
+else { #COUNTY_PROVIDES_PRECINCT_DATA is FALSE
+    if (any (!is.null(county_provided_data_names)))
+    stop("COUNTY_PROVIDED_ file, precinct location name column, or precinct address columns must be null 
+        if COUNTY_PROVIDES_PRECINCT_DATA is FALSE")
+
 
 ########
 # For testing
